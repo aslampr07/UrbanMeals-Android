cf08c90a73e1c6cf80474d2ad6a5db8155cea509
Aslam Rasheed <aslampr07@outlook.com>
2018-12-02 14:27:27 +0530

    Endpoints are moved to new package

---
+++ app/src/main/java/com/urbanmeals/client/urbanmeals/services/EndPoints.kt
@@ -0,0 +1,9 @@
+package com.urbanmeals.client.urbanmeals.services
+
+class EndPoints {
+
+    private val HOSTNAME = "urbanmeals.in"
+    //private val HOSTNAME = "192.168.0.100:8000"
+
+    val login = "http://$HOSTNAME/api/1.0/account/login"
+}
