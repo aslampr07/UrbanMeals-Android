51ce3c897298155a9244802489e25a7b27932281
Aslam Rasheed <aslampr07@outlook.com>
2018-12-14 21:55:17 +0530

    Code cleanup removed all older items

--- app/src/main/java/com/urbanmeals/client/urbanmeals/interfaces/CardListReadyListener.java
+++
@@ -1,9 +0,0 @@
-package com.urbanmeals.client.urbanmeals.interfaces;
-
-import com.android.volley.VolleyError;
-import com.urbanmeals.client.urbanmeals.adapters.HomeHotelListAdapter;
-
-public interface CardListReadyListener {
-    void onCompleted(HomeHotelListAdapter adapter);
-    void onError(VolleyError error);
-}
--- app/src/main/java/com/urbanmeals/client/urbanmeals/interfaces/LocationReadyListener.java
+++
@@ -1,7 +0,0 @@
-package com.urbanmeals.client.urbanmeals.interfaces;
-
-import android.location.Location;
-
-public interface LocationReadyListener {
-    void onResult(Location location);
-}
